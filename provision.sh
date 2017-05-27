13db73fa454e27eac45e7cc1e54bcad2eadac36b
gomesuit <gomesuit@gmail.com>
2017-05-27 17:36:47 +0900

    svae

--- provision.sh
+++ provision.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-apt-get update
+#apt-get update
 #apt-get dist-update -y
-
 
 service postgresql start
 update-rc.d postgresql enable
@@ -12,19 +11,18 @@ update-rc.d postgresql enable
 
 apt-get install gedit -y
 
-
 echo “spool /root/msf_console.log” > /root/.msf4/msfconsole.rc
 
 # The Backdoor Factory
-git clone https://github.com/secretsquirrel/the-backdoor-factory /opt/the-backdoor-factory
+#git clone https://github.com/secretsquirrel/the-backdoor-factory /opt/the-backdoor-factory
 # 対話型になるのでコメントアウト
 # cd /opt/the-backdoor-factory
 # ./install.sh
 
 # HTTPScreenShot
-pip install selenium
-git clone https://github.com/breenmachine/httpscreenshot.git /opt/httpscreenshot
-cd /opt/httpscreenshot
-chmod +x install-dependencies.sh && ./install-dependencies.sh
+#pip install selenium
+#git clone https://github.com/breenmachine/httpscreenshot.git /opt/httpscreenshot
+#cd /opt/httpscreenshot
+#chmod +x install-dependencies.sh && ./install-dependencies.sh
 
 
